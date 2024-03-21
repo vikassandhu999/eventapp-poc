be0426543cc511feefa3bd178335f37c1d2ec9ed
vikas sandhu <vikassandhu999@gmail.com>
2024-03-21 21:02:54 +0530

    poc

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { PrimeReactProvider } from "primereact/api";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <PrimeReactProvider value={{ unstyled: true, pt: {} }}>
+      <App />
+    </PrimeReactProvider>
+  </React.StrictMode>
+);
